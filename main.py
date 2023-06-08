593d6c2c076c4484863978329b36f7125c985e1f
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2023-06-08 15:56:22 -0600

    01 function print

---
+++ main.py
@@ -0,0 +1,9 @@
+# Comentarios con gato.
+# function print
+print("Hola DSM 3B")
+
+#Se pueden enviar distintos tipos de datos
+print("Un String")
+print("un entero", 25)
+print("un float", 3.14)
+print("Un boolean", True)
