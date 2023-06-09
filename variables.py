1d875ad829c3a0f8bc5149684d0a3d6434893b87
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2023-06-09 13:24:12 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Python lenguaje interpretado no tipado (es decir, los tipos de variables no se declaran porque se reconocen en automático)
+
+name="Derek Aguilar"
+age=39
+height=1.72
+is_student=True
+children = [] # Este formato es para listas
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(children))
