fe84251264223a606ca09807901c1cc64d665f61
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2023-06-12 09:53:09 -0600

    05 Operadores Logicos y Booleanos

---
+++ operadores_2.py
@@ -0,0 +1,23 @@
+# Operadores booleanos
+# > >=
+print(10>10)
+print(10>=10)
+# < <=
+print(10<10)
+print(10<=10)
+# ==
+print(10==10)
+# !=
+print(10!=10)
+
+# Operadores Lógicos
+# and (True if all True)
+print(True and True)
+print(True and False)
+# or (True if ot least one True)
+print(True or True)
+print(True or False)
+print(False or False)
+# not (not True)
+print(not True)
+print(not False)
