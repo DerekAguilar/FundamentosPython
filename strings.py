600c0a13780431566d1151aa449c40db21931ae6
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2023-06-13 14:16:54 -0600

    07 Strings

---
+++ strings.py
@@ -0,0 +1,29 @@
+myString=" hOla mUnDo! "
+
+# Mayúsculas
+print(myString.upper())
+
+# Minúsculas
+print(myString.lower())
+
+#Longitud de String
+print(len(myString))
+
+# Quitar espacios al inicio y final
+print(myString.strip())
+
+myString="Este es un nuevo texto"
+
+# Indexing and slicing
+print(myString[0])
+print(myString[3])
+print(myString[6])
+
+print(myString[-1])
+print(myString[-2])
+
+# Slicing
+# [inicio:fin]
+print(myString[0:5])
+print(myString[2:5])
+print(myString[5:])
